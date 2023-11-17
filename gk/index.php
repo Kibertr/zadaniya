3641c6b818bdc5844754843dd389e52dddd65e3f
Kibertr <118417803+Kibertr@users.noreply.github.com>
2023-11-17 12:31:24 +0300

    Create index.php

---
+++ gk/index.php
@@ -0,0 +1,79 @@
+<?php
+include("main.php");
+$cl = new database();
+?>
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+    <head>
+        <script src="assets/js/color-modes.js"></script>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <title>Offcanvas navbar template · Bootstrap v5.3</title>
+        <link rel="stylesheet" href="downloads/css@3.css">
+        <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="offcanvas-navbar.css" rel="stylesheet">
+
+        <style>
+        </style>
+    </head>
+  <body class="bg-body-tertiary">
+    <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
+      <symbol id="check2" viewBox="0 0 16 16">
+        <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
+      </symbol>
+      <symbol id="circle-half" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z"/>
+      </symbol>
+      <symbol id="moon-stars-fill" viewBox="0 0 16 16">
+        <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/>
+        <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z"/>
+      </symbol>
+      <symbol id="sun-fill" viewBox="0 0 16 16">
+        <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
+      </symbol>
+    </svg>
+
+<main class="container">
+<div class="d-flex align-items-center p-3 my-3 text-white bg-purple rounded shadow-sm">
+    <div class="lh-1">
+    <form method="post" action="add.php">
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Почта</label>
+        <input type="email" name="mail" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Текст</label>
+        <input type="text" name="text" class="form-control" id="exampleInputPassword1">
+      </div>
+        <input type="hidden" name="date" value="<?php echo date('Y-m-d'); ?>">
+        <input type="hidden" name="ip" value="<?php echo $_SERVER['REMOTE_ADDR'];?>">
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+</div>
+
+  <div class="my-3 p-3 bg-body rounded shadow-sm">
+    <h6 class="border-bottom pb-2 mb-0">Гостевая книга</h6>
+    <?php
+        $array = $cl->getdata();
+        foreach($array as $key=>$val){
+              echo '<div class="d-flex text-body-secondary pt-3">';
+              echo '<svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>';
+              echo '<p class="pb-3 mb-0 small lh-sm border-bottom" style="width:100%;">';
+              echo '<strong class="d-block text-gray-dark">'.$val["mail"]."<text style='float:right;'>".$val['ip']."</text></strong>";
+              echo '<br>';
+              echo '<text>'.$val['text'].'</text>';
+              echo '<strong class="d-block text-gray-dark" style="float:right;">'.$val["date"]."</strong>";
+              echo "</p>";
+              echo '</div>';
+        }
+    ?>
+    
+  </div>
+</main>
+<script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+
+    <script src="offcanvas-navbar.js"></script></body>
+</html>
