62c7e4decc73b8977c900786aeef3c8e7a189a6e
Kibertr <118417803+Kibertr@users.noreply.github.com>
2023-11-17 12:32:32 +0300

    Update main.php

--- gk/main.php
+++ gk/main.php
@@ -1,1 +1,18 @@
+<?php
+class database{
+    private $conn = "";
 
+    function __construct(){
+        $this->conn = mysqli_connect("localhost","root","root","zadanie");
+
+    }
+    public function getdata(){
+        $sql = "SELECT * FROM gk";
+        $result = mysqli_query($this->conn,$sql);
+        while ($row = mysqli_fetch_assoc($result)){
+            $array[]=$row;
+        }
+        return $array;
+    }
+}
+?>
