41e766101c0bdc31a279c21e450c66b295d72364
Kibertr <118417803+Kibertr@users.noreply.github.com>
2023-11-17 13:57:24 +0300

    Update main.php

--- gk/main.php
+++ gk/main.php
@@ -6,13 +6,30 @@ class database{
         $this->conn = mysqli_connect("localhost","root","root","zadanie");
 
     }
-    public function getdata(){
-        $sql = "SELECT * FROM gk";
+    public function pagination(){
+        $sql = "SELECT COUNT(*) FROM gk";
+        $result = mysqli_query($this->conn,$sql);
+        $row = mysqli_fetch_row($result);
+        return $row[0];
+    }
+
+    public function getdata($nachalo){
+        $sql = "SELECT * FROM gk LIMIT $nachalo,10";
         $result = mysqli_query($this->conn,$sql);
         while ($row = mysqli_fetch_assoc($result)){
             $array[]=$row;
         }
         return $array;
+    }
+    public function plus($stranica,$stranic){
+        if ($stranica>=$stranic){
+        }else $stranica++;
+        return $stranica;
+    }
+    public function minus($stranica){
+        if ($stranica==1){
+        }else $stranica--;
+        return $stranica;
     }
 }
 ?>
