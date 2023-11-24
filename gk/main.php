25a2384f910c2ee50ee2e63b6fad9403765ddc1c
Kibertr <118417803+Kibertr@users.noreply.github.com>
2023-11-24 10:26:25 +0300

    Update main.php

--- gk/main.php
+++ gk/main.php
@@ -1,9 +1,12 @@
 <?php
+session_start();
+
 class database{
     private $conn = "";
 
     function __construct(){
         $this->conn = mysqli_connect("localhost","root","root","zadanie");
+
 
     }
     public function pagination(){
@@ -13,13 +16,15 @@ class database{
         return $row[0];
     }
 
-    public function getdata($nachalo){
-        $sql = "SELECT * FROM gk LIMIT $nachalo,10";
+    public function getdata($nachalo,$limit){
+        $sql = "SELECT * FROM gk ORDER BY date DESC LIMIT $nachalo,$limit ";
         $result = mysqli_query($this->conn,$sql);
-        while ($row = mysqli_fetch_assoc($result)){
-            $array[]=$row;
+        if(mysqli_num_rows($result)>0){
+            while ($row = mysqli_fetch_assoc($result)){
+                $array[]=$row;
+            }
+            return $array;
         }
-        return $array;
     }
     public function plus($stranica,$stranic){
         if ($stranica>=$stranic){
@@ -31,5 +36,18 @@ class database{
         }else $stranica--;
         return $stranica;
     }
+
+    function add(){
+        $mail = $_POST['mail'];
+        $text = $_POST['text'];
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $date = date('Y-m-d H-i-s');
+        $sql = "INSERT INTO `gk`(`mail`, `text`, `ip`, `date`) VALUES ('$mail','$text','$ip','$date')";
+        $result = mysqli_query($this->conn,$sql);
+    }
+}
+if(!empty($_POST['mail']) && !empty($_POST['text'])){
+    $cl = new database();
+    $cl->add();
 }
 ?>
